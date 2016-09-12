f3f36997704a228c40702f7d0dfa90e69b5bd36f
Ate <poortinga.ate@gmail.com>
2016-09-12 16:02:03 +0700

    fix polygon handler

--- server.py
+++ server.py
@@ -172,13 +172,17 @@ class PolygonHandler(webapp2.RequestHandler):
 		
 		content = ComputePolygonDrawTimeSeries(mypoly,ref_start,ref_end,series_start,series_end)
 		
-		print type(content)
+		thelist = []
+		for items in content:
+			thelist.append([int(items[0]),float(items[1])])
+		
+		
 		#self.response.headers['Content-Type'] = 'application/json'
 		#self.response.out.write(content)
 		#self.response.headers['Content-Type'] = 'application/json'   
 
 		self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
-		self.response.write(content)
+		self.response.write(thelist)
 		#self.response.write(coords);
 		#self.response.headers['Content-Type'] = 'application/json'
 		
