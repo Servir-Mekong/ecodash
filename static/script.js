e1d1e91a8712141eef587690e22da735256a4c19
Ate <poortinga.ate@gmail.com>
2016-09-12 14:53:04 +0700

    fix polygon handler

--- static/script.js
+++ static/script.js
@@ -396,6 +396,8 @@ ecodash.App.prototype.initButton = function(map,provinceNames,countryNames) {
 				myName.push("my area " + counter.toString());
 				console.log("show the chart");
 				console.log(data.length);
+				var array = data.split(',');
+				console.log(array);
 				//ecodash.App.prototype.showChart(data);
 				ecodash.App.prototype.HideProgress();
 			}}).bind(this));
