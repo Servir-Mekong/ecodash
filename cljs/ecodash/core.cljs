fdba4f80ea0404d2d6c7fa781a1ddfc285de0f7e
Gary Johnson <gwjohnso@uvm.edu>
2016-09-22 19:07:47 -0400

    Hijacked the div#ecodash element to use as the toplevel container for my Reagent SPA.

--- cljs/ecodash/core.cljs
+++ cljs/ecodash/core.cljs
@@ -2,5 +2,19 @@
   (:require [goog.dom :as dom]
             [reagent.core :as r]))
 
+(defn page-body []
+  [:header
+   [:div#logos
+    [:img#usaid  {:src "/static/images/usaid.png"}]
+    [:img#nasa   {:src "/static/images/nasa.png"}]
+    [:img#google {:src "/static/images/googlelogo_color_272x92dp.png"}]
+    [:img#adpc   {:src "/static/images/adpclogo.jpg"}]
+    [:img#servir {:src "/static/images/servir.png"}]]
+   [:nav
+    [:ul
+     [:li {:on-click #(js/alert "Clicked Home")} "Home"]
+     [:li {:on-click #(js/alert "Clicked About")} "About"]
+     [:li {:on-click #(js/alert "Clicked Application")} "Application"]]]])
+
 (defn ^:export main []
-  (js/alert "Loaded ecodash.core!"))
+  (r/render [page-body] (dom/getElement "ecodash")))
