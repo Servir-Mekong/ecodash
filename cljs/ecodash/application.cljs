d013a797ffe6b1292e0309f8f7f5a59b4d9c48fd
Gary Johnson <gwjohnso@uvm.edu>
2016-09-23 15:52:40 -0400

    Renamed UI header, enabled info button popup, and added instructions and legend.

--- cljs/ecodash/application.cljs
+++ cljs/ecodash/application.cljs
@@ -1,31 +1,66 @@
 (ns ecodash.application
-  (:require [goog.dom :as dom]
-            [goog.string :as str]
+  (:require [goog.string :as str]
             [reagent.core :as r]))
 
-(defonce visible-controls (r/atom :ui))
+(defonce visible-controls (r/atom #{:ui}))
+
+(defn visible-control? [control]
+  (contains? @visible-controls control))
 
 (defn get-display-style [control]
-  (if (= @visible-controls control)
+  (if (visible-control? control)
     {:display "block"}
     {:display "none"}))
+
+(defn show-control! [control]
+  (swap! visible-controls conj control))
+
+(defn hide-control! [control]
+  (swap! visible-controls disj control))
 
 (defn content []
   [:div#ecodash
    [:div#map]
-   [:div#ui
-    {:style (get-display-style :ui)}
-    [:input#counter {:type "hidden" :name "counter" :value 0}]
+   [:div#ui {:style (get-display-style :ui)}
     [:header
-     [:h1 "Eco Dash"]
-     [:div#collapse-button
-      {:on-click #(reset! visible-controls :settings-button)}
+     [:h1 "Map Controls"]
+     [:div#collapse-button {:on-click (fn []
+                                        (hide-control! :ui)
+                                        (show-control! :settings-button))}
       (str/unescapeEntities "&#171;")]
-     [:input#info-button {:type "button" :name "info-button" :value "i"}]]]
-   [:div#settings-button
-    {:style (get-display-style :settings-button)
-     :on-click #(reset! visible-controls :ui)}
-    (str/unescapeEntities "&#9776;")]])
+     [:input#info-button {:type "button" :name "info-button" :value "i"
+                          :on-click (fn []
+                                      (if (visible-control? :info)
+                                        (hide-control! :info)
+                                        (show-control! :info)))}]]
+    [:p#instructions
+     "Select a time period to set the baseline, the study period and"
+     " perform a new calculation."]]
+   [:div#settings-button {:style (get-display-style :settings-button)
+                          :on-click (fn []
+                                      (hide-control! :settings-button)
+                                      (show-control! :ui))}
+    (str/unescapeEntities "&#9776;")]
+   [:div#general-info {:style (get-display-style :info)}
+    [:h1 "Welcome to the Eco Dash"]
+    [:p
+     "Lorem ipsum dolor sit amet, nam et ac, ligula viverra dictumst"
+     " turpis, nibh et vitae duis pulvinar sodales. Nullam viverra voluptas"
+     " sit, pede pellentesque pellentesque dui suspendisse. Praesent quisque"
+     " ultricies eget in purus viverra, elit neque mauris pretium ac, dolor"
+     " massa in ac nulla, tempus per a vel eros vivamus arcu, elit dui mauris."]]
+   [:div#legend
+    [:h2 "Legend"]
+    [:table
+     [:tbody
+      [:tr
+       [:td {:row-span "3"} [:img {:src "/static/images/mylegend.png"}]]
+       [:td "Increase"]]
+      [:tr
+       [:td "Stable"]]
+      [:tr
+       [:td "Decrease"]]]]]
+   [:input#counter {:type "hidden" :name "counter" :value "0"}]])
 
 (defonce country (google.maps.Data.))
 (defonce province (google.maps.Data.))
