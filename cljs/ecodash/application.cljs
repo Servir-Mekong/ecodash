129994301a29e0460bc66919b96b0e5935c51f48
Gary Johnson <gwjohnso@uvm.edu>
2016-09-23 12:40:43 -0400

    Logged the Python variables and added the page structure for the Application page.

--- cljs/ecodash/application.cljs
+++ cljs/ecodash/application.cljs
@@ -1,15 +1,40 @@
 (ns ecodash.application
   (:require [goog.dom :as dom]
+            [goog.string :as str]
             [reagent.core :as r]))
 
+(defonce visible-controls (r/atom :ui))
+
+(defn get-display-style [control]
+  (if (= @visible-controls control)
+    {:display "block"}
+    {:display "none"}))
+
 (defn content []
-  [:h1 "Application"])
+  [:div#ecodash
+   [:div#map]
+   [:div#ui
+    {:style (get-display-style :ui)}
+    [:input#counter {:type "hidden" :name "counter" :value 0}]
+    [:header
+     [:h1 "Eco Dash"]
+     [:div#collapse-button
+      {:on-click #(reset! visible-controls :settings-button)}
+      (str/unescapeEntities "&#171;")]
+     [:input#info-button {:type "button" :name "info-button" :value "i"}]]]
+   [:div#settings-button
+    {:style (get-display-style :settings-button)
+     :on-click #(reset! visible-controls :ui)}
+    (str/unescapeEntities "&#9776;")]])
 
 (defonce country (google.maps.Data.))
 (defonce province (google.maps.Data.))
 
+(defn log [& vals]
+  (.log js/console (apply str vals)))
+
 (defn init [ee-map-id ee-token country-polygons province-polygons]
-  (js/alert ee-map-id)
-  (js/alert ee-token)
-  (js/alert country-polygons)
-  (js/alert province-polygons))
+  (log ee-map-id)
+  (log ee-token)
+  (log country-polygons)
+  (log province-polygons))
